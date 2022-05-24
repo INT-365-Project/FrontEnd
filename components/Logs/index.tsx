a306330edeffa693c9d624f3950e7962bbeafd51
Peempat Pinsang <peempat.pinsang@mail.kmutt.ac.th>
2022-05-24 18:39:22 +0700

    [update] connect service get report message

--- components/Logs/index.tsx
+++ components/Logs/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-// import dateFormat from "dateformat";
+import { useState } from "react";
+import { useEffect } from "react";
+import ReportServices from '../../services/report';
 const data = [
   { title: "ค่าเทอมลดมั้ยครับช่วงโควิดแล้วลดกี่เปอเซ็นหรอครับจริงครับๆ", published_at: "2021-9-10" },
   { title: "ค่าเทอมลดมั้ยครับช่วงโควิด", published_at: "2021-11-10" },
@@ -12,19 +14,26 @@ const data = [
   { title: "ค่าเทอมลดมั้ยครับช่วงโควิด", published_at: "2021-6-10" },
   { title: "ค่าเทอมลดมั้ยครับช่วงโควิด", published_at: "2021-7-10" },
 ];
-
 const Logs = () => {
+  const [logs,setLogs] = useState([]);
+  useEffect(()=>{
+    ReportServices.getReport().then((res) => {
+      setLogs(res.data.responseData);
+    })
+    .catch((err) => {
+      console.log(err.response);
+    });
+  },[])
   return (
     <div id="news-card" className="w-full lg:w-[95%] ">
       <div className="text-purple subtitle">Logs</div>
       <div className="rounded-xl drop-shadow-lg bg-white mt-[10px] mx-auto h-[400px] overflow-hidden ">
         <div className="pl-[10px] text-[14px] lg:text-[16px] lg:pl-[30px] pr-[20px] overflow-y-auto h-[330px] w-[90%] mt-[30px] mx-auto text-warmGray-500 ">
-          {data.map((l,index) => {
-            // const publishedAt = new Date(l.published_at);
+          {logs.map((l,index) => {
             return (
               <div key={index} className="flex justify-between pt-[20px] py-[5px] h-[70px]short-sub-title">
-                <p className="w-[50%]">{l.title}</p>
-                <p >{l.published_at}</p>
+                <p className="w-[50%]">{l.topic}</p>
+                <p >{l.createDate.slice(0,10)}</p>
                 {/* <p>{dateFormat(publishedAt, "dd mmm yy")}</p> */}
               </div>
             );
