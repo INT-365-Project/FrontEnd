484220fd394bb78233c7e98dbe6a0db641b3801a
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2022-07-14 17:32:46 +0700

    [Formatting] : format code

--- services/news.ts
+++ services/news.ts
@@ -1,32 +1,32 @@
 import { api } from "../config";
 import axios from "axios";
 let getToken = null
-if (typeof window !== 'undefined'){
-  getToken = localStorage.getItem('accessToken') 
+if (typeof window !== 'undefined') {
+  getToken = localStorage.getItem('accessToken')
 }
-let token = 'Bearer '+ getToken
+let token = 'Bearer ' + getToken
 
 const NewsService = axios.create({
-  baseURL:`${api}`
+  baseURL: `${api}`
 })
 
 
 
 const NewsServices = {
-  sendPathImage:(filePath:any)=>
-  NewsService.post("/api/viewFileByPath",filePath),
-  getNews:()=> NewsService.get("/api/news"),
-  getNewsById:(slug:any)=> NewsService.get(`/api/news/`,{params:{newsId:slug}}),
-  removeNewsById:(slug:any)=> NewsService.delete('/api/news/deleteNews/',{
-    headers:{
-      Authorization:token
+  sendPathImage: (filePath: any) =>
+    NewsService.post("/api/viewFileByPath", filePath),
+  getNews: () => NewsService.get("/api/news"),
+  getNewsById: (slug: any) => NewsService.get(`/api/news/`, { params: { newsId: slug } }),
+  removeNewsById: (slug: any) => NewsService.delete('/api/news/deleteNews/', {
+    headers: {
+      Authorization: token
     },
-    params:{
-      newsId:slug
+    params: {
+      newsId: slug
     }
   }),
-  storeNews: (data:{title:string;detail:string; thumbnailPath:string ; thumbnailFile:string;  thumbnailFileName:string})=>
-   NewsService.post("/api/news/createOrUpdateNews",data,{headers:{Authorization:token}})
+  storeNews: (data: { title: string; detail: string; thumbnailPath: string; thumbnailFile: string; thumbnailFileName: string }) =>
+    NewsService.post("/api/news/createOrUpdateNews", data, { headers: { Authorization: token } })
 }
 
 export default NewsServices;
