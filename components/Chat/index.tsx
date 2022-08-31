1613b57638ea63429359336ef21539d153f34de3
Peempat Pinsang <peempat.pinsang@mail.kmutt.ac.th>
2022-08-31 18:03:58 +0700

    [update] redesign chat

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -157,7 +157,7 @@ const Chat = () => {
 
   return (
 
-    <div className="px-[10px] lg:px-0  lg:pl-[130px] pt-[80px] lg:pt-[0] lg:pr-[50px] w-full">
+    <div className="px-[10px] bg-[#F8F8F8] lg:px-0  lg:pl-[130px] pt-[80px] lg:pt-[90px] lg:pr-[50px] w-full">
       <Head>
         <title>Chat</title>
         <meta name="Chat" content="Chat" />
@@ -165,12 +165,9 @@ const Chat = () => {
       </Head>
       {userData.connected ? (
         <main className="lg:min-h-screen relative w-full pt-[30px]">
-          <div className="w-full">
-            <div>
-              <h1 className="title">Chat</h1>
-              <h2 className="breadcrumb">
-                <a>Home</a> | Chat
-              </h2>
+          <div className="w-full bg-white ">
+            <div className="h-[70px] flex items-center pl-[25px] rounded-[10px]">
+              <h1 className="text-[24px] text-black font-bold">ข้อความทั้งหมด</h1>
             </div>
           </div>
           <div className="mt-[15px] block lg:hidden w-full mx-auto h-[30px]">
@@ -179,30 +176,31 @@ const Chat = () => {
                 setIsOpen(false);
               }}
               className={`${
-                isOpen ? "bg-purple text-white" : "text-purple border-purple"
+                isOpen ? "bg-[#336699] text-white" : "text-[#336699] border-[#336699]"
               } border-[1.6px]  p-2 rounded-xl w-full lg:w-[92%]`}
             >
               Back
             </button>
           </div>
+          {/* Contact */}
           <div className=" min-h-[80vh] mt-[35px] flex flex-col lg:flex-row">
             <div className="lg:w-[30%] lg:h-auto">
               <div
                 id="desktop"
                 className="w-full lg:w-[93%] lg:h-full bg-white rounded-3xl drop-shadow-md"
               >
-                <div className="lg:flex hidden justify-center px-[30px] pb-[10px] h-[40px] ">
+                <div className="lg:flex hidden justify-center px-[30px] pb-[10px] h-[60px]  border-b border-[#919191]-[0.8px]">
                   <button
                     onClick={() => setButtonClick(true)}
-                    className={`${"border-t-4 text-purple"} border-purple w-full `}
+                    className={`${"border-t-4 text-[#336699]"} border-[#336699] w-full `}
                   >
                     Message
                   </button>
                 </div>
-                {!isOpen && <div className="flex lg:hidden justify-center px-[30px] pb-[10px] h-[40px] ">
+                {!isOpen && <div className="flex lg:hidden justify-center items-center px-[30px] pb-[10px] h-[40px] ">
                   <button
                     onClick={() => setButtonClick(true)}
-                    className={`${"border-t-4 text-purple"} border-purple w-full `}
+                    className={`${"border-t-4 text-[#336699]"} border-[#336699] w-full `}
                   >
                     Message
                   </button>
@@ -264,12 +262,13 @@ const Chat = () => {
                         </li>
                       ))}
                     </ul>
+                    {/* Contact */}
                   </div>
                 ) : (
                   <>
                     {tab == "CHATROOM" && (
-                      <div className="pt-[30px] lg:hidden space-y-[10px] flex flex-col h-[400px]   pb-[20px]">
-                        <div className="border-[1px] overflow-y-scroll h-full border-black mx-[4px]">
+                      <div className="pt-[30px] lg:hidden space-y-[10px] flex flex-col h-[400px] pb-[20px]">
+                        <div className="border-[1px] overflow-y-scroll h-full border-[#336699] rounded-[15px]  mx-[4px]">
                           {publicChats.map((chat, index) => (
                             <li
                               className={`message ${
@@ -280,7 +279,7 @@ const Chat = () => {
                               {chat.senderName !== userData.username && (
                                 <div className="avatar">{chat.senderName}</div>
                               )}
-                              <div className="message-data">{chat.message}</div>
+                              <div className={`message-data w-[280px] break-words ${chat.senderName === userData.username && "self" ? 'text-right' : 'text-left'}`}>{chat.message}</div>
                               {chat.senderName === userData.username && (
                                 <div className="avatar self">
                                   {chat.senderName}
@@ -292,8 +291,8 @@ const Chat = () => {
                       </div>
                     )}
                     {tab !== "CHATROOM" && (
-                      <div className="pt-[30px] lg:hidden space-y-[10px] flex flex-col  h-[360px]  pb-[20px]">
-                        <div className="border-[1px] overflow-y-scroll h-full border-black mx-[4px]">
+                      <div className="pt-[30px] lg:hidden space-y-[10px] flex flex-col  h-[360px]  pb-[20px] ">
+                        <div className="border-[1px] overflow-y-scroll h-full border-[#336699] rounded-[15px] mx-[4px]">
                           {[...privateChats.get(tab)].map((chat, index) => (
                             <li
                               className={`message ${
@@ -304,7 +303,7 @@ const Chat = () => {
                               {chat.senderName !== userData.username && (
                                 <div className="avatar">{chat.senderName}</div>
                               )}
-                              <div className="message-data">{chat.message}</div>
+                              <div className={`message-data w-[280px] break-words ${chat.senderName === userData.username && "self" ? 'text-right' : 'text-left'}`}>{chat.message}</div>
                               {chat.senderName === userData.username && (
                                 <div className="avatar self">
                                   {chat.senderName}
@@ -322,15 +321,16 @@ const Chat = () => {
                 <>
                   {tab == "CHATROOM" && (
                     <div className="h-[15%] w-full lg:w-[93%]  lg:hidden bg-white  rounded-xl drop-shadow-md flex items-center mt-[20px]">
-                      <input
-                        className="text-[14px] pl-[30px]   ml-[4px] border-[2px] border-warmGray-200  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
+                      <textarea
+                        rows={2}
+                        className="text-[14px] pl-[30px]   ml-[4px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="ข้อความ ...."
                         value={userData.message}
                         onChange={handleMessage}
                       />
                       <button
                         onClick={sendValue}
-                        className="w-[30%] lg:w-[10%] mx-auto h-[30px] mr-[10px] text-white bg-purple rounded-lg  "
+                        className="w-[30%] lg:w-[10%] mx-auto h-[30px] mr-[10px] text-white bg-[#336699] rounded-lg  "
                       >
                         Send
                       </button>
@@ -338,15 +338,16 @@ const Chat = () => {
                   )}
                   {tab !== "CHATROOM" && (
                     <div className="h-[15%] w-full lg:w-[93%]  lg:hidden bg-white rounded-xl drop-shadow-md flex items-center mt-[20px]">
-                      <input
-                        className="text-[14px] pl-[30px]   ml-[4px] border-[2px] border-warmGray-200 rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
+                      <textarea
+                        className="text-[14px] pl-[30px] ml-[4px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        rows={2}
+                        placeholder="ข้อความ ...."
                         value={userData.message}
                         onChange={handleMessage}
                       />
                       <button
                         onClick={sendPrivateValue}
-                        className="w-[30%] lg:w-[10%] mx-auto h-[30px] mr-[10px] text-white bg-purple rounded-lg  "
+                        className="w-[30%] lg:w-[10%] mx-auto h-[30px] mr-[10px] text-white bg-[#336699] rounded-lg  "
                       >
                         Send
                       </button>
@@ -358,12 +359,12 @@ const Chat = () => {
             <div id="mobile" className="hidden lg:w-[70%] lg:block ">
               <div className=" h-[85%] ">
                 <div className="h-[95%] bg-white rounded-3xl drop-shadow-md  px-[20px]">
-                  <div className="h-[15%] mx-auto border-b-[1.5px] flex items-center">
+                  <div className="h-[15%] mx-auto flex items-center">
                     {tab && tab}
                   </div>
                   {tab === "CHATROOM" && (
                     <div className="h-[400px]">
-                      <ul className="overflow-y-auto h-[90%] border-[1px] border-black">
+                      <ul className="overflow-y-auto h-[90%] border-[1px] border-[#336699] rounded-[15px]">
                         {publicChats.map((chat, index) => (
                           <li
                             className={`message ${
@@ -374,7 +375,7 @@ const Chat = () => {
                             {chat.senderName !== userData.username && (
                               <div className="avatar">{chat.senderName}</div>
                             )}
-                            <div className="message-data">{chat.message}</div>
+                            <div className={`message-data w-[280px] break-words ${chat.senderName === userData.username && "self" ? 'text-right' : 'text-left'}`}>{chat.message}</div>
                             {chat.senderName === userData.username && (
                               <div className="avatar self">
                                 {chat.senderName}
@@ -387,7 +388,7 @@ const Chat = () => {
                   )}
                   {tab !== "CHATROOM" && (
                     <div className="h-[400px]">
-                      <ul className="overflow-y-auto h-[90%] border-[1px] border-black">
+                      <ul className="overflow-y-auto h-[90%] border-[1px] border-[#336699] rounded-[15px]">
                         {[...privateChats.get(tab)].map((chat, index) => (
                           <li
                             className={`message ${
@@ -398,7 +399,7 @@ const Chat = () => {
                             {chat.senderName !== userData.username && (
                               <div className="avatar">{chat.senderName}</div>
                             )}
-                            <div className="message-data">{chat.message}</div>
+                            <div className={`message-data w-[280px] break-words ${chat.senderName === userData.username && "self" ? 'text-right' : 'text-left'}`}>{chat.message}</div>
                             {chat.senderName === userData.username && (
                               <div className="avatar self">
                                 {chat.senderName}
@@ -413,15 +414,16 @@ const Chat = () => {
               </div>
               {tab === "CHATROOM" && (
                 <div className="h-[15%] bg-white rounded-3xl drop-shadow-md flex items-center">
-                  <input
-                    className="text-[14px] pl-[30px] ml-[4px] border-[2px] border-warmGray-200 mt-[6px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
+                  <textarea
+                    className="text-[14px] pl-[30px] ml-[4px] mt-[6px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    rows={2}
+                    placeholder="ข้อความ ....."
                     value={userData.message}
                     onChange={handleMessage}
                   />
                   <button
                     onClick={sendValue}
-                    className="w-[8%] mx-auto h-[30px] text-white bg-purple rounded-lg  "
+                    className="w-[8%] mx-auto h-[30px] text-white bg-[#336699] rounded-lg  "
                   >
                     Send
                   </button>
@@ -429,15 +431,16 @@ const Chat = () => {
               )}
               {tab !== "CHATROOM" && (
                 <div className="h-[15%] bg-white rounded-3xl drop-shadow-md flex items-center">
-                  <input
-                    className="text-[14px] pl-[30px] ml-[4px] border-[2px] border-warmGray-200  mt-[6px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
+                  <textarea
+                    rows={2}
+                    className="text-[14px] pl-[30px] ml-[4px]  mt-[6px]  rounded w-[90%] py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="ข้อความ ....."
                     value={userData.message}
                     onChange={handleMessage}
                   />
                   <button
                     onClick={sendPrivateValue}
-                    className="w-[8%] mx-auto h-[30px] text-white bg-purple rounded-lg  "
+                    className="w-[8%] mx-auto h-[30px] text-white bg-[#336699] rounded-lg  "
                   >
                     Send
                   </button>
@@ -447,19 +450,21 @@ const Chat = () => {
           </div>
         </main>
       ) : (
-        <div className=" px-[10px] lg:px-0 flex flex-col pt-[80px] sm:justify-center sm:items-center space-y-[10px] lg:pl-[130px]  lg:pt-[40px] lg:pr-[50px] w-full h-screen">
-          <div><label>Chat room</label></div>
-          <div className="flex items-center space-x-2"><input
-          className="rounded-lg p-2"
+        <div className="h-screen pt-[80px]">
+          <div className="flex flex-col justify-center items-center">
+          <div className="bg-white rounded-[10px] w-[90%] h-[60px] flex items-center pl-[25px] font-bold shadow-lg"><label>Chat Room</label></div>
+          <div className="flex items-center space-x-2 pt-[10px] w-[90%]"><input
+          className="rounded-lg p-2 w-[60%] shadow-lg"
             id="user-name"
             placeholder="Enter your name"
             name="userName"
             value={userData.username}
             onChange={handleUsername}
           />
-          <button className="w-[40%] mx-auto h-[30px] text-white bg-purple rounded-lg" type="button" onClick={registerUser}>
+          <button className="w-[40%]  mx-auto text-white bg-[#336699] p-2  my-auto rounded-lg shadow-lg" type="button" onClick={registerUser}>
             connect
           </button>
+        </div>
         </div>
         </div>
       )}
