4abd95abc55084b611617813932043aa9b900f1b
Peempat Pinsang <peempat.pinsang@mail.kmutt.ac.th>
2022-09-06 13:22:04 +0700

    [update] select filter mobile bug

--- pages/report/index.tsx
+++ pages/report/index.tsx
@@ -117,7 +117,7 @@ const Report = () => {
           </div>
         </div>
         <div className="block sm:hidden mt-[20px] md:ml-[100px] bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 md:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <select className="w-full">
+          <select className="w-full" onChange={(e) => sort(e.target.value)}>
             <option value="asc">Select</option>
             <option value="asc">เรียงจากล่าสุด</option>
             <option value="desc">เรียกจากเก่าสุด</option>
